9350b1f2ac590a76478a1a5dc5b753e334ddef84
thibautbaissac <tbaissac@gmail.com>
2021-12-21 13:52:59 +0100

    Create seed and add container to application layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+# DESTOY ALL
+puts "Destroying all Movies"
+Movie.destroy_all
+
+puts "--------"
+
+# CREATE WORKS
+100.times do
+  movie = Movie.new(title: Faker::Movie.title,
+  quote: Faker::Movie.quote,
+  rating: Random.rand(1..5),
+  release_date: Faker::Date.between(from: '1950-01-01', to: Date.today))
+  movie.save!
+  puts "Created movie : #{movie.title}"
+end
+
+
+puts "--------"
+puts "Successful seed creation"
